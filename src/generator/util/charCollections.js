bee57a76da8ae4c58366e9f34069496dd72fd841
Jarne <dev@jarne.rocks>
2025-03-14 15:21:57 +0100

    Add available characters

---
+++ src/generator/util/charCollections.js
@@ -0,0 +1,8 @@
+/**
+ * PassPanda | char collections
+ */
+
+export const UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+export const LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+export const NUMBERS = "0123456789"
+export const SPECIAL_CHARS = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/"
